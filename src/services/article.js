c949e5231d498b4811821423642c6ca3adacaca4
Sergiy Antonyuk <s.p.antonyuk@gmail.com>
2023-04-27 00:33:26 +0300

    ref: store

--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
-console.log('rapidApiKey: ', rapidApiKey);
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
@@ -16,8 +15,6 @@ export const articleApi = createApi({
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            // encodeURIComponent() function encodes special characters that may be present in the parameter values
-            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
             query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
         }),
     }),
